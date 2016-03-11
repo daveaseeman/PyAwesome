32c1963fabbc60d18ac40e5ba4793d950315ccd3
shantnu <shantnu@users.noreply.github.com>
2016-03-11 14:13:43 +0000

    Fixing a few errors, libs now install

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install python3-pip -y
 
 
-sudo apt-get install libxml2-dev libxslt1-dev python-dev -y
+sudo apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev -y
 
-sudo pip3 install lxml python-docx
+ sudo pip3 install lxml python-docx
