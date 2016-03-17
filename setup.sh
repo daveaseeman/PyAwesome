32fb4b053015dc35d3de05090bc6b849c41f49c2
shantnu <shantnu@users.noreply.github.com>
2016-03-17 17:51:19 +0000

    Trying playing with pdf and doc files

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ sudo apt-get install python3-pip -y
 
 sudo apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev -y
 
- sudo pip3 install lxml python-docx
+sudo pip3 install lxml python-docx
+sudo pip3 install pypdf2
