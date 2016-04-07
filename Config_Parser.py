7270b19073c20d4cea0c79a832b3a97e915c9aad
shantnu <shantnu@users.noreply.github.com>
2016-04-07 17:06:14 +0100

    Adding config parser and environ reader

--- Config_Parser.py
+++ Config_Parser.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 import configparser
+import pdb
+
 config = configparser.ConfigParser()
 
-configFilePath = r'config.cfg'
-print(config.read(configFilePath))
+configFilePath = 'config.cfg'
+
+config.read(configFilePath)
+server = config.get('main_config', 'server')
+flag = config.get('main_config', 'flag')
+ports = config.get('main_config', 'ports')
+
+port_split = ports.split(",")
+
+print(server, flag, ports)
+
+
+cmd = "ssh_mine " + server
+
+if flag == 'True': # explain why. bool(string) is always true!!!
+    cmd += " --save_session "
+
+for port in port_split:
+    cmd += " --port " + port + " "
+
+print("The final command that will be run is: ")    
+print(cmd)
