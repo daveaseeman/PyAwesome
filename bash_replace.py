cca39c911bb0c02d327974f344a7c6f8d0790900
shantnu <shantnu@users.noreply.github.com>
2016-04-18 09:55:58 +0100

    adding logging

--- bash_replace.py
+++ bash_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# As user1 
+# yes > /dev/null 
+
 from subprocess import Popen, PIPE
 import pdb
 
@@ -11,6 +14,7 @@ cpu_line = stdout.split("\n")[7]
 print(cpu_line)
 cpu = cpu_line.split()[8]
 user = cpu_line.split()[1]
+command = cpu_line.split()[11]
 
 print("Top cpu loading is: ",cpu)
 
@@ -20,4 +24,7 @@ if float(cpu) > 90:
     print(stdout)
 
     # add logging
+    with open("errors.log", "a") as f:
+        f.write("\nKilling command {} from user {}, because taking {} % CPU\n".format(command, user, cpu))
+
 
