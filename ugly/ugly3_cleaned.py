7bcf5bc3a87b6a136d087b4efc75497123c8d0b4
shantnu <shantnu@users.noreply.github.com>
2016-04-25 17:10:09 +0100

    Clean up ugly code

---
+++ ugly/ugly3_cleaned.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#******************************************************
+# * Copyright (C) 2016
+# * 
+# * This file is part of Project Ugly.
+# * 
+# * Code can not be copied and/or distributed without the express
+# * permission of Acme Co
+# *******************************************************
+
+
+def ugly3Print():
+    print("This is ugly 3")
+    
+ugly3Print()    
