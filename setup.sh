a8750e3d5bef2ab75e801761212e16a05d84d853
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-06-16 11:54:20 +0100

    Revert "Revert "add git attrtibutes""
    This reverts commit 02634ab4c5af1535db201d0fc1ec54dd13dfa34e.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 sudo apt-get update
-sudo apt-get install python3-pip -y
+#sudo apt-get install python3-pip -y
 
 
-sudo apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev libfreetype6-dev libxft-dev cython -y
+sudo apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev libfreetype6-dev libxft-dev cython tree -y
 
-sudo pip3 install lxml python-docx
-sudo pip3 install pypdf2
-sudo pip3 install jinja2
-
-
-sudo pip3 install numpy
-sudo apt-get install tree 
 
 cd /tmp
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b
 ~/miniconda3/bin/conda install numba -y
+
+~/miniconda3/bin/pip install /vagrant/req.txt
+echo "cd /vagrant" >> /home/vagrant/.bashrc
+cd /vagrant
